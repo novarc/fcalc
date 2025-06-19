4136ce6c0f8b448a7360eede46e83b77007c2d77
Arjun G. Menon <contact@arjungmenon.com>
2025-06-19 07:01:56 -0400

    Starting on tokenize...

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,64 @@ enum Token {
 }
 
 fn tokenize(line: &str) -> Vec<Token> {
-    Vec::new()
+    let mut tokens: Vec<Token> = Vec::new();
+
+    #[derive(Debug)]
+    enum TokenChars {
+        Whitespace(Vec<char>),
+        Number(Vec<char>),
+        Symbol(Vec<char>),
+        // String(Vec<char>),
+        // Comment(Vec<char>),
+    }
+
+    let mut token_chars_collection: Vec<TokenChars> = Vec::new();
+    let mut current_token_chars: Option<TokenChars> = None;
+
+    fn determine_token_kind(ch: char) -> TokenChars {
+        if ch.is_alphabetic() {
+            TokenChars::Symbol(vec![ch])
+        } else if ch.is_numeric() {
+            TokenChars::Number(vec![ch])
+        } else if ch.is_whitespace() {
+            TokenChars::Whitespace(vec![ch])
+        } else {
+            todo!("Unknown token kind char: {}", ch)
+        }
+    }
+
+    for ch in line.chars() {
+        
+
+        match current_token_chars {
+            None => {
+                current_token_chars = Some(determine_token_kind(ch));
+            }
+            Some(ref mut token_chars) => {
+                match token_chars {
+                    TokenChars::Whitespace(chars) => {
+                        chars.push(ch);
+                    }
+                    TokenChars::Number(chars) => {
+                        chars.push(ch);
+                    }
+                    TokenChars::Symbol(chars) => {
+                        chars.push(ch);
+                    }
+                }
+            }
+        }
+        
+        
+    }
+    
+    if let Some(token_chars) = current_token_chars {    
+        token_chars_collection.push(token_chars);
+    }
+
+    println!("{:?}", token_chars_collection);
+
+    tokens
 }
 
 fn run(line: &str) {
