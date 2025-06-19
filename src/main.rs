5b2b10d8d66fb0c432117e4607b0975efb7e57a9
Arjun G. Menon <contact@arjungmenon.com>
2025-06-19 06:23:06 -0400

    Specify some token types

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,46 @@
 use rustyline;
 
+#[derive(Debug)]
+struct WhiteSpace {
+    whitespace: String
+}
 
+#[derive(Debug)]
+struct Integer {
+    value: i64
+}
+
+#[derive(Debug)]
+struct RealNumber {
+    value: f64
+}
+
+#[derive(Debug)]
+enum Number {
+    Integer(Integer),
+    RealNumber(RealNumber)
+}
+
+#[derive(Debug)]
+struct Symbol {
+    value: String
+}
+
+#[derive(Debug)]
+enum Token {
+    Whitespace(WhiteSpace),
+    Number(Number),
+    Symbol(Symbol)
+}
+
+fn tokenize(line: &str) -> Vec<Token> {
+    Vec::new()
+}
 
 fn run(line: &str) {
     println!("{}", line);
+    let tokens = tokenize(line);
+    println!("{:?}", tokens);
 }
 
 fn main() {
