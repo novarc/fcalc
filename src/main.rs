9f923662c7ff52943811f8cb3aaa4131bf5920e3
Arjun G. Menon <contact@arjungmenon.com>
2025-06-26 16:21:15 -0400

    Refactor inkwell example a bit

--- src/main.rs
+++ src/main.rs
@@ -13,42 +13,21 @@ use inkwell::module::Module;
 
 use std::error::Error;
 
-/// Convenience type alias for the `sum` function.
-///
-/// Calling this is innately `unsafe` because there's no guarantee it doesn't
-/// do `unsafe` operations internally.
-type SumFunc = unsafe extern "C" fn(u64, u64, u64) -> u64;
+fn inkwell_example() -> Result<(), Box<dyn Error>> {
+	/// Convenience type alias for the `sum` function.
+	/// Calling this is innately `unsafe` because there's no guarantee it doesn't
+	/// do `unsafe` operations internally.
+	type SumFunc = unsafe extern "C" fn(u64, u64, u64) -> u64;
 
-struct CodeGen<'ctx> {
-	context: &'ctx Context,
-	module: Module<'ctx>,
-	builder: Builder<'ctx>,
-	execution_engine: ExecutionEngine<'ctx>,
-}
-
-impl<'ctx> CodeGen<'ctx> {
-	fn jit_compile_sum(&self) -> Option<JitFunction<'_, SumFunc>> {
-		let i64_type = self.context.i64_type();
-		let fn_type = i64_type.fn_type(&[i64_type.into(), i64_type.into(), i64_type.into()], false);
-		let function = self.module.add_function("sum", fn_type, None);
-		let basic_block = self.context.append_basic_block(function, "entry");
-
-		self.builder.position_at_end(basic_block);
-
-		let x = function.get_nth_param(0)?.into_int_value();
-		let y = function.get_nth_param(1)?.into_int_value();
-		let z = function.get_nth_param(2)?.into_int_value();
-
-		let sum = self.builder.build_int_add(x, y, "sum").unwrap();
-		let sum = self.builder.build_int_add(sum, z, "sum").unwrap();
-
-		self.builder.build_return(Some(&sum)).unwrap();
-
-		unsafe { self.execution_engine.get_function("sum").ok() }
+	/// A struct that contains the context, module, builder, and execution engine.
+	/// This is used to generate the LLVM IR for the `sum` function.
+	struct CodeGen<'ctx> {
+		context: &'ctx Context,
+		module: Module<'ctx>,
+		builder: Builder<'ctx>,
+		execution_engine: ExecutionEngine<'ctx>,
 	}
-}
 
-fn main2() -> Result<(), Box<dyn Error>> {
 	let context = Context::create();
 	let module = context.create_module("sum");
 	let execution_engine = module.create_jit_execution_engine(OptimizationLevel::None)?;
@@ -59,9 +38,35 @@ fn main2() -> Result<(), Box<dyn Error>> {
 		execution_engine,
 	};
 
-	let sum = codegen
-		.jit_compile_sum()
-		.ok_or("Unable to JIT compile `sum`")?;
+	let i64_type = codegen.context.i64_type();
+	let fn_type = i64_type.fn_type(&[i64_type.into(), i64_type.into(), i64_type.into()], false);
+	let function = codegen.module.add_function("sum", fn_type, None);
+	let basic_block = codegen.context.append_basic_block(function, "entry");
+
+	codegen.builder.position_at_end(basic_block);
+
+	let x = function
+		.get_nth_param(0)
+		.ok_or("param 0 missing")?
+		.into_int_value();
+	let y = function
+		.get_nth_param(1)
+		.ok_or("param 1 missing")?
+		.into_int_value();
+	let z = function
+		.get_nth_param(2)
+		.ok_or("param 2 missing")?
+		.into_int_value();
+
+	let sum = codegen.builder.build_int_add(x, y, "sum").unwrap();
+	let sum = codegen.builder.build_int_add(sum, z, "sum").unwrap();
+
+	codegen.builder.build_return(Some(&sum)).unwrap();
+
+	let llvm_fn: Option<JitFunction<'_, SumFunc>> =
+		unsafe { codegen.execution_engine.get_function("sum").ok() };
+
+	let sum = llvm_fn.ok_or("Unable to JIT compile `sum`")?;
 
 	let x = 1u64;
 	let y = 2u64;
@@ -219,7 +224,7 @@ fn main() {
 
 	// let _ = repl();
 
-	let _ = main2();
+	let _ = inkwell_example();
 }
 
 #[allow(dead_code)]
